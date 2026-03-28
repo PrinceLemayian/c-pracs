95c01c9b5275e99ec564bbdab369d1226c8915f0
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-03-28 22:32:38 +0300

    add program to learn i/o and variables

---
+++ program2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  float gpa = 2.5;
+  float price = 19.99;
+  float temperature = -10.1;
+
+  printf("Your GPA is %f\n", gpa);
+  printf("The price is $%f\n", price);
+  printf("The temperature is %f\n", temperature);
+
+  return 0;
+}
