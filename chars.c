807e9d54e54c1ecfbf03dab8c4799fbe255cd334
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-03-29 16:00:29 +0300

    program to learn chars in C

---
+++ chars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  char grade = 'F';
+  char symbol = '!';
+  char currency = '$';
+
+  printf("Your grade is %c\n", grade);
+  printf("Your favourite symbol is %c\n", symbol);
+  printf("The currency is %c\n", currency);
+
+  return 0;
+}
