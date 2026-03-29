d1e31b9f20d12251ce13de1c89949e6e36286327
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-03-29 20:36:09 +0300

    add program to learn strings in C

---
+++ strings.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  char name[] = "Lemayian";
+  char food[] = "Githeri";
+  char email[] = "fake123@gmail.com";
+
+  printf("Hello %s\n", name);
+  printf("Your favourite food is %s\n", food);
+  printf("Your email is %s\n", email);
+
+  return 0;
+}
