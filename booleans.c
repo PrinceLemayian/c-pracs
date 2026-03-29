7e128cef93797aeb1085e7d40c43a5291d5055a9
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-03-30 02:14:07 +0300

    program to practice booleans in C

--- booleans.c
+++ booleans.c
@@ -2,10 +2,32 @@
 #include <stdio.h>
 
 int main() {
+
+  // (false == 0)
+  // (true == 1)
   bool isOnline = true;
-
+  bool isStudent = false;
+  bool forSale = true;
   // prints 1
-  printf("%d", isOnline);
+  // printf("%d", isOnline);
 
+  if (isOnline) {
+    printf("You are ONLINE\n");
+  } else {
+    printf("You are OFFLINE\n");
+  }
+
+  if (isStudent) {
+    printf("You are a student\n");
+  } else {
+    printf("You are NOT a student\n");
+  }
+
+  if (forSale) {
+    printf("That item is for sale\n");
+
+  } else {
+    printf("That item is NOT for sale\n");
+  }
   return 0;
 }
