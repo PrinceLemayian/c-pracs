d32ce441f0daed023f44e0b350185cda2a0be4f6
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-03-31 22:31:32 +0300

    add program to practice arithmetic operators in C

---
+++ arithmeticOperators.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+  int x = 2;
+  int y = 3;
+  int z = 0;
+  int a = 4;
+
+  // Addition
+  // z = x + y;
+  // Subtraction
+  // z = x - y;
+  // Multiplication
+  // z = x * y;
+  // Division
+  // z = a / x;
+  // Modulus
+  z = y % x;
+
+  printf("%d\n", z);
+
+  return 0;
+}
