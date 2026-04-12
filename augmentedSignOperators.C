6ca3a3d9b6e686f7d94afe38aea9ae77ebedbfe3
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-12 23:33:35 +0300

    add examples for augmented sign operators in C

---
+++ augmentedSignOperators.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  int x = 2;
+
+  // Addition
+  x += 2;
+
+  // Subtraction
+  x -= 2;
+
+  // Multiplication
+  x *= 3;
+
+  // Division
+  x /= 3;
+
+  printf("%d", x);
+
+  return 0;
+}
