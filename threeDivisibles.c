9f8b9a80bacf0bdf437d04c0324c8ed5207dad85
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-22 15:22:39 +0300

    add program to display first 20 numbers divisible by 3

---
+++ threeDivisibles.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int count = 0;
+  int num = 1;
+
+  while (count < 20) {
+    if (num % 3 == 0) {
+      printf("%d\n", num);
+      count++;
+    }
+    num++;
+  }
+  return 0;
+}
