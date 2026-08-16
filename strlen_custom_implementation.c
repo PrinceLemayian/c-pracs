ddcf47a18a87f8030001625b02c9a11b085acc29
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-08-16 22:53:41 +0300

    feat: improved implementation using pointer arithmetic

--- strlen_custom_implementation.c
+++ strlen_custom_implementation.c
@@ -10,10 +10,20 @@ int count_length(const char *str) {
   return count;
 }
 
+int my_strlen(char *str) {
+  char *p = str;
+
+  while (*p != '\0') {
+    p++;
+  }
+
+  return p - str;
+}
+
 int main(void) {
   char name[] = "Lemayian";
 
-  printf("The length of your string is %d\n", count_length(name));
+  printf("The length of your string is %d\n", my_strlen(name));
 
   return 0;
 }
